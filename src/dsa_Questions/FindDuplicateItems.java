a0853a2d2abd309412dfc385b12aa25aa32a571b
Rohan Uke <ukerohan7@gmail.com>
2025-06-12 22:41:19 +0530

    Day 43: Solved duplicate number detection using brute force approach in Java

---
+++ src/dsa_Questions/FindDuplicateItems.java
@@ -0,0 +1,24 @@
+package dsa_Questions;
+
+public class FindDuplicateItems {
+    public static void main(String[] args) {
+        int[] arr = {3, 1, 4, 2, 5, 3};
+
+        boolean found = false;
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[i] == arr[j]) {
+                    System.out.println("Duplicate element found: " + arr[i]);
+                    found = true;
+                    break;
+                }
+            }
+            if (found) break;
+        }
+
+        if (!found) {
+            System.out.println("No duplicate found");
+        }
+    }
+}
