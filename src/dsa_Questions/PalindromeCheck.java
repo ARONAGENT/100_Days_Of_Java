3f6f789979e984ad5e19d2d04fc8bd0b4e6f3f15
Rohan Uke <ukerohan7@gmail.com>
2025-06-17 23:22:15 +0530

    ✅ Day 48: Implemented palindrome check using two-pointer technique in Java

---
+++ src/dsa_Questions/PalindromeCheck.java
@@ -0,0 +1,27 @@
+package dsa_Questions;
+
+public class PalindromeCheck {
+    public static boolean isPalindrome(String str) {
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                return false; // Not a palindrome
+            }
+            left++;
+            right--;
+        }
+        return true; // It's a palindrome
+    }
+
+    public static void main(String[] args) {
+        String word = "nayan";
+
+        if (isPalindrome(word)) {
+            System.out.println(word + " is a palindrome.");
+        } else {
+            System.out.println(word + " is not a palindrome.");
+        }
+    }
+}
