991f0255cb2c3eca93a5183f04c4572312ea2697
Rohan Uke <ukerohan7@gmail.com>
2025-07-01 00:12:02 +0530

    Day- 56 Learn Comparable and Campartor Difference and when to used

---
+++ src/Basic_Questions/ComparatorDemo.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class ComparatorDemo {
+    public static void main(String[] args) {
+        List<Student> students = List.of(
+                new Student(3, "Om"),
+                new Student(1, "Rohan"),
+                new Student(2, "Yash")
+        );
+
+        // 🔸 Using Comparable (by id)
+        List<Student> list1 = new ArrayList<>(students);
+        Collections.sort(list1);
+        System.out.println("Sorted by ID: " + list1);
+
+        // 🔸 Using Comparator (by name)
+        List<Student> list2 = new ArrayList<>(students);
+        list2.sort(Comparator.comparing(student -> student.name));
+        System.out.println("Sorted by Name: " + list2);
+    }
+}
+// Comparable tab use hota hai jab aap chahte ho ki object khud decide kare wo kaise compare hoga.
+//Comparator tab use hota hai jab aap bahar se comparison ka rule dena chahte ho (like sort by name, age).
+//Interview mein mostly puchte hain: "When will you use Comparable vs Comparator?"
