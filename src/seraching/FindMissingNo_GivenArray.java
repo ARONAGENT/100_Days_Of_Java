9fafe7fbfb6b28f71bd394622dc1911562f0a2eb
Rohan Uke <ukerohan7@gmail.com>
2025-07-10 14:53:16 +0530

    Update program

--- src/seraching/FindMissingNo_GivenArray.java
+++ src/seraching/FindMissingNo_GivenArray.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class FindMissingNo_GivenArray {
     public static void main(String[] args) {
         int[] arr={1,2,3,4,5,6,8};
-        int n= arr[arr.length-1];
+        int n= arr.length+1;
         int expectedSum= n*(n+1)/2;
         int actual_sum=0;
         for(int num : arr){
@@ -13,6 +13,7 @@ public class FindMissingNo_GivenArray {
         }
 
         System.out.println(actual_sum);
+        System.out.println(expectedSum);
         int missing_no= expectedSum-actual_sum;
         System.out.println("missing no :"+missing_no);
 
