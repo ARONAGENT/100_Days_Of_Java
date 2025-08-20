1be7aea918d5350e17c45243f3039ce5f8762b26
Rohan Uke <ukerohan7@gmail.com>
2025-08-20 23:46:09 +0530

    Day 98: Explored Java Generics with Generic Methods, including print, compare, and getMax examples

---
+++ src/Basic_Questions/GenericMethod.java
@@ -0,0 +1,19 @@
+package Basic_Questions;
+
+public class GenericMethod {
+    // Generic method
+    public static <T> void printArray(T[] array) {
+        for (T element : array) {
+            System.out.print(element + " ");
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Integer[] intArr = {1, 2, 3, 4};
+        String[] strArr = {"A", "B", "C"};
+
+        printArray(intArr);
+        printArray(strArr);
+    }
+}
